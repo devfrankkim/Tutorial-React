6a9cdfb19d5df258dee9dcae6b48d6faf4478bcc
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-01-29 13:03:06 -0500

    Books Component

---
+++ src/Books.js
@@ -0,0 +1,23 @@
+export const book = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._SX355_BO1,204,203,200_.jpg",
+    title: "Brown Bear, Brown Bear, What Do You See?",
+    author: "Frank Kim",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51wfzW0QnXL._SX355_BO1,204,203,200_.jpg",
+    title: "Chicka Chicka Boom Boom",
+    author: "Frank",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51wfzW0QnXL._SX355_BO1,204,203,200_.jpg",
+    title: "Chicka Chicka Boom Boom",
+    author: "Kim",
+  },
+];
