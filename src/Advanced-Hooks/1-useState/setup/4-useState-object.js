1f44f922bf06c37fbc2e879e72133c9d14ba5213
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-02-03 22:50:26 -0500

    useState with Objects -> spread operator OR use multiple values

--- src/Advanced-Hooks/1-useState/setup/4-useState-object.js
+++ src/Advanced-Hooks/1-useState/setup/4-useState-object.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 
 // use spread operator because setState function is going to wipe out the whole Object
+// Use an Object OR separate multiple values
+// Make sure you preserve values from the Object (spread operator)
 const UseStateObject = () => {
   const [person, setPerson] = useState({
     name: "frank",
     age: 24,
     message: "hey frank smile",
   });
+
+  const [name, setName] = useState("Frank");
+  const [age, setAge] = useState("30");
+  const [message, setMessage] = useState("What is your job?");
+
   const addJobTitle = () => {
     setPerson({ ...person, job: "software developer" });
   };
   console.log(person);
   return (
     <>
-      <h4>{person.name}</h4>
-      <h4>{person.age}</h4>
-      <h4>{person.message}</h4>
-      <h4>{person.job}</h4>
       <h4>{"Hello"}</h4>
+      {/* <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.message}</h4> */}
+      <h4>{name}</h4>
+      <h4>{age}</h4>
+      <h4>{person.message}</h4>
+      <h4>{message}</h4>
+      <h4>{person.job}</h4>
       <button className="btn" onClick={addJobTitle}>
         Add job title
       </button>
