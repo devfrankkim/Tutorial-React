babfd18f4caaac4757f5bac3f2c8ccfe413a59f5
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2021-04-04 20:44:42 -0400

    useRef

---
+++ src/Advanced-Hooks/5-useRef/setup/1-useRef-basics.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+
+// preserves value
+// DOES NOT trigger re-render
+// target DOM nodes/elements
+
+const UseRefBasics = () => {
+  const refContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+  };
+  useEffect(() => {
+    console.log(refContainer.current);
+    console.log(refContainer.current.value);
+    // refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+        </div>
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+};
+
+export default UseRefBasics;
